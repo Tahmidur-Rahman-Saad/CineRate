6cc8803bfdbfcb46369d3405e299849a93621492
tahmidur_saad <tahmidursaad@gmail.com>
2025-01-15 12:56:25 +0600

    add models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Director(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='customers', null = True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    Nationality = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='director_images/', null=True, blank=True)
+
+
+    
+
+
+class Cast(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='customers', null = True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    Nationality = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='director_images/', null=True, blank=True)
+    gender = models.BooleanField(default=None)
+
+
+
+class Authorizer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='customers', null = True)
+    phone = models.CharField(max_length=15,null=True,blank=True)
+    nid = models.CharField(max_length=15,null = True,blank=True)
+    image = models.ImageField(upload_to='authorizer_images/', null=True, blank=True)
+
+
+
+
+class Reviewer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='customers', null = True)
+    image = models.ImageField(upload_to='authorizer_images/', null=True, blank=True)
+    age = models.IntegerField(default=18)
+    gender = models.BooleanField(default=None)
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from accounts.models import Cast,Director
 
 # Create your models here.
+
+class Movie(models.Model):
+    CRITERIA_CHOICES = [
+        ('U', 'Universal'),
+        ('PG', 'Parental Guidance'),
+        ('12A', '12A'),
+        ('15', '15'),
+        ('18', '18'),
+    ]
+
+    FINAL_VERDICT_CHOICES = [
+        ('F', 'Flop'),
+        ('A', 'Average'),
+        ('H', 'Hit'),
+        ('B', 'Blockbuster'),
+    ]
+
+
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    criteria = models.CharField(max_length=10,choices=CRITERIA_CHOICES)
+    language = models.CharField(max_length=50)
+    discription = models.TextField(max_length=200)
+    budget = models.FloatField(max_length=10,help_text="Enter in Lakh")
+    total_collection = models.FloatField(max_length=10,help_text="Enter in Lakh")
+    final_verdict = models.CharField(max_length=10,choices=FINAL_VERDICT_CHOICES)
+    image = models.ImageField(upload_to='movie_images/', null=True, blank=True)
+    casts = models.ManyToManyField(Cast, related_name='movies')
+    director = models.OneToOneField(Director,on_delete=models.CASCADE, related_name='director', null = True)
+
+
+
+    
+class Music(models.Model):
+    name = models.CharField(max_length=100 )
+    singers = models.TextField(max_length=50)
+    publish_date = models.DateField()
+    lyrics = models.CharField(max_length=50)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE,null=None)
