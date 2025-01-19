b4ee0e2292c558658fbd4c4c758b11d7c4b15a74
tahmidur_saad <tahmidursaad@gmail.com>
2025-01-19 12:26:15 +0600

    add urls path for movies

--- movies/urls.py
+++ movies/urls.py
@@ -2,6 +2,15 @@ from django.urls import path
 from . import views  
 
 urlpatterns = [
-    path('some-path/', views.some_view, name='some_view'),
+    path('', views.movies, name='movies'),
+    path('movie-details/<str:pk>/', views.movieDetails, name='movie-details'),
+    path('movie-create/', views.movieCreate, name='movie-create'),
+    path('movie-update/<str:pk>/', views.movieUpdate, name='movie-update'),
+    path('movie-delete/<str:pk>/', views.movieDelete, name='movie-delete'),
+    path('movie-search/<str:pk>/', views.movieSearch, name='movie-search'),
+    path('movie-top-10-rating-last-week/', views.movieTopTenRatingLastWeek, name='movie-top-10-rating-last-week'),
+    path('movie-top-10-rating-last-month/', views.movieTopTenRatingLastMonth, name='movie-top-10-rating-last-month'),
+    path('movie-top-10-rating-last-year/', views.movieTopTenRatingLastYear, name='movie-top-10-rating-last-year'),
+    path('movie-search-by-language/', views.movieSearchByLanguage, name='movie-search-by-language'),
     
 ]
--- movies/views.py
+++ movies/views.py
@@ -5,9 +5,53 @@ from rest_framework.decorators import api_view
 
 # Create your views here.
 @api_view(['GET'])
-def some_view(request):
-    return Response({
-                'code': status.HTTP_200_OK,
-                'response': "Data Received Successfully",
-               
-        })
+def movies(request):
+    pass
+
+@api_view(['GET'])
+def movieDetails(request,pk):
+    pass
+
+
+@api_view(['POST'])
+def movieCreate(request):
+    pass
+
+@api_view(['PATCH'])
+def movieUpdate(request,pk):
+    pass
+
+
+@api_view(['DELETE'])
+def movieDelete(request,pk):
+    pass
+
+
+@api_view(['GET'])
+def movieSearch(request):
+    pass
+
+
+@api_view(['GET'])
+def movieTopTenRatingLastWeek(request):
+    pass
+
+
+
+@api_view(['GET'])
+def movieTopTenRatingLastMonth(request):
+    pass
+
+
+
+@api_view(['GET'])
+def movieTopTenRatingLastYear(request):
+    pass
+
+
+
+@api_view(['GET'])
+def movieSearchByLanguage(request,language):
+    pass
+
+
